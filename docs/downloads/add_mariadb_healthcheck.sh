72529b693325a26c5fe514783e8ad4bb30ec7f76
Christoph Dyllick-Brenzinger <cdb@datamate.org>
2024-02-10 15:11:18 +0100

    fix issues with add_mariadb_healthcheck.sh

--- docs/downloads/add_mariadb_healthcheck.sh
+++ docs/downloads/add_mariadb_healthcheck.sh
@@ -22,13 +22,8 @@ echo "> I try to get the SEATABLE_MYSQL_ROOT_PASSWORD from your .env file."
 source /opt/seatable-compose/.env
 echo "> Your SEATABLE_MYSQL_ROOT_PASSWORD is '${SEATABLE_MYSQL_ROOT_PASSWORD}'."
 
-echo "> I try to create the healthcheck user in the database"
-docker exec -i ${CONTAINER_NAME} mysql -u root -p${SEATABLE_MYSQL_ROOT_PASSWORD} -e "CREATE USER 'healthcheck'@'127.0.0.1' IDENTIFIED BY '${HEALTHCHECK_PASS}';" && \
-docker exec -i ${CONTAINER_NAME} mysql -u root -p${SEATABLE_MYSQL_ROOT_PASSWORD} -e "GRANT USAGE ON *.* TO 'healthcheck'@'127.0.0.1';" && \
-docker exec -i ${CONTAINER_NAME} mysql -u root -p${SEATABLE_MYSQL_ROOT_PASSWORD} -e "CREATE USER 'healthcheck'@'::1' IDENTIFIED BY '${HEALTHCHECK_PASS}';" && \
-docker exec -i ${CONTAINER_NAME} mysql -u root -p${SEATABLE_MYSQL_ROOT_PASSWORD} -e "GRANT USAGE ON *.* TO 'healthcheck'@'::1';" && \
-docker exec -i ${CONTAINER_NAME} mysql -u root -p${SEATABLE_MYSQL_ROOT_PASSWORD} -e "CREATE USER 'healthcheck'@'localhost' IDENTIFIED BY '${HEALTHCHECK_PASS}';" && \
-docker exec -i ${CONTAINER_NAME} mysql -u root -p${SEATABLE_MYSQL_ROOT_PASSWORD} -e "GRANT USAGE ON *.* TO 'healthcheck'@'localhost';"
+echo "> I try to create the healthcheck user in the database."
+CHECK_USER=$(docker exec -i ${CONTAINER_NAME} mysql -u root -p${SEATABLE_MYSQL_ROOT_PASSWORD} -e "SELECT * FROM mysql.user WHERE user = 'healthcheck';")
 
 if [ $? -eq 1 ]; then
   echo "> I have a problem: it seems that the container is either not running or I can not write the user in the database."
@@ -36,20 +31,30 @@ if [ $? -eq 1 ]; then
   exit 1
 fi
 
-if [ -e ${CONTAINER_DATADIR}/.my-healthcheck.cnf ]; then
-  echo "> I have a problem: it seems that there is already a file with the healthcheck credentials at ${CONTAINER_DATADIR}/.my-healthcheck.cnf. I will not overwrite this..."
-  echo "> Script not successful."
-  exit 1
+if [ -z "${CHECK_USER}" ]; then
+    docker exec -i ${CONTAINER_NAME} mysql -u root -p${SEATABLE_MYSQL_ROOT_PASSWORD} -e "CREATE USER 'healthcheck'@'127.0.0.1' IDENTIFIED BY '${HEALTHCHECK_PASS}';" && \
+    docker exec -i ${CONTAINER_NAME} mysql -u root -p${SEATABLE_MYSQL_ROOT_PASSWORD} -e "GRANT USAGE ON *.* TO 'healthcheck'@'127.0.0.1';" && \
+    docker exec -i ${CONTAINER_NAME} mysql -u root -p${SEATABLE_MYSQL_ROOT_PASSWORD} -e "CREATE USER 'healthcheck'@'::1' IDENTIFIED BY '${HEALTHCHECK_PASS}';" && \
+    docker exec -i ${CONTAINER_NAME} mysql -u root -p${SEATABLE_MYSQL_ROOT_PASSWORD} -e "GRANT USAGE ON *.* TO 'healthcheck'@'::1';" && \
+    docker exec -i ${CONTAINER_NAME} mysql -u root -p${SEATABLE_MYSQL_ROOT_PASSWORD} -e "CREATE USER 'healthcheck'@'localhost' IDENTIFIED BY '${HEALTHCHECK_PASS}';" && \
+    docker exec -i ${CONTAINER_NAME} mysql -u root -p${SEATABLE_MYSQL_ROOT_PASSWORD} -e "GRANT USAGE ON *.* TO 'healthcheck'@'localhost';"
+    echo "> I created the healthcheck user in the database."
 else
-  echo "> I write the .my-healtcheck.cnf file now."
-cat <<EOF > ${CONTAINER_DATADIR}/.my-healthcheck.cnf
-[mariadb-client]
-port=3306
-socket=/run/mysqld/mysqld.sock
-user=healthcheck
-password=${HEALTHCHECK_PASS}
-protocol=tcp
-EOF
+    echo "> There is already a healthcheck user in the database. Check if ${CONTAINER_DATADIR}/.my-healthcheck.cnf exists."
+fi
+
+if [ -e ${CONTAINER_DATADIR}/.my-healthcheck.cnf ]; then
+    echo "> I have a problem: it seems that there is already a file with the healthcheck credentials at ${CONTAINER_DATADIR}/.my-healthcheck.cnf. I will not overwrite this..."
+    echo "> Script not successful."
+    exit 1
+else
+    echo "> I write the .my-healtcheck.cnf file now."
+    echo "[mariadb-client]" > ${CONTAINER_DATADIR}/.my-healthcheck.cnf
+    echo "port=3306" >> ${CONTAINER_DATADIR}/.my-healthcheck.cnf
+    echo "socket=/run/mysqld/mysqld.sock" >> ${CONTAINER_DATADIR}/.my-healthcheck.cnf
+    echo "user=healthcheck" >> ${CONTAINER_DATADIR}/.my-healthcheck.cnf
+    echo "password=${HEALTHCHECK_PASS}" >> ${CONTAINER_DATADIR}/.my-healthcheck.cnf
+    echo "protocol=tcp" >> ${CONTAINER_DATADIR}/.my-healthcheck.cnf
 fi
 
 echo "> FINISH SUCCESSFUL. Your container should become healthy now."
